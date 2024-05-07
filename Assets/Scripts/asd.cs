8e34c08cc3dee061d2427fa25ec9530345e376cd
Charly0400 <Carlaz020400@gmail.com>
2024-05-07 13:05:36 -0600

    tarea 1

--- Assets/Scripts/asd.cs
+++ Assets/Scripts/asd.cs
@@ -4,59 +4,68 @@ using UnityEngine;
 using UnityEngine.XR.Content.Interaction;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
+using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class asd : MonoBehaviour {
-    public class RotatingObject : MonoBehaviour {
-        public XRBaseInteractable knobInteractable; // El XR Knob que controlará la rotación
-        public float rotationSpeed = 10f; // Velocidad de rotación
 
-        private bool isInteracting = false; // Bandera para saber si se está interactuando con el knob
+    public XRBaseInteractable knobInteractable; // El XR Knob que controlará la rotación
+    public float rotationSpeed = 10f; // Velocidad de rotación
 
-        void Update() {
-            if (isInteracting) {
-                //float rotationAmount = knobInteractable.selectingInteractor.transform.localRotation.z * rotationSpeed * Time.deltaTime;
+    private bool isInteracting = false; // Bandera para saber si se está interactuando con el knob
+
+    void Update() {
+        if (isInteracting) {
+            if (knobInteractable.interactorsSelecting.Count > 0) {
+                // Obtenemos el primer interactor seleccionando
+                //XRBaseInteractor interactor = knobInteractable.interactorsSelecting[0];
+
+                // Calculamos la rotación basada en el movimiento del interactor
+                //float rotationAmount = interactor.transform.localRotation.z * rotationSpeed * Time.deltaTime;
+
+                // Aplicamos la rotación al objeto
                 //transform.Rotate(Vector3.up, rotationAmount);
             }
         }
-
-        private void OnEnable() {
-            knobInteractable.selectEntered.AddListener(OnSelectEnter);
-            knobInteractable.selectExited.AddListener(OnSelectExit);
-        }
-
-        private void OnDisable() {
-            knobInteractable.selectEntered.RemoveListener(OnSelectEnter);
-            knobInteractable.selectExited.RemoveListener(OnSelectExit);
-        }
-
-        void OnSelectEnter(SelectEnterEventArgs args) {
-            isInteracting = true;
-        }
-
-        void OnSelectExit(SelectExitEventArgs args) {
-            isInteracting = false;
-        }
-        /*/public GameObject cubo;
-        public XRKnob ruedita;
-        public float value_Ruedita;
-        //public Transform ruedita;
-        public Transform cubito;
-
-        // Start is called before the first frame update
-        void Start()
-        {
-            value_Ruedita = ruedita.value;
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-            cubito.rotation.SetAxisAngle(cubito.rotation, value_Ruedita);
-        }
-
-        public void Funtion() {
-
-        }
-        */
     }
+
+    private void OnEnable() {
+        knobInteractable.selectEntered.AddListener(OnSelectEnter);
+        knobInteractable.selectExited.AddListener(OnSelectExit);
+    }
+
+    private void OnDisable() {
+        knobInteractable.selectEntered.RemoveListener(OnSelectEnter);
+        knobInteractable.selectExited.RemoveListener(OnSelectExit);
+    }
+
+    void OnSelectEnter(SelectEnterEventArgs args) {
+        isInteracting = true;
+    }
+
+    void OnSelectExit(SelectExitEventArgs args) {
+        isInteracting = false;
+    }
+    /*/public GameObject cubo;
+    public XRKnob ruedita;
+    public float value_Ruedita;
+    //public Transform ruedita;
+    public Transform cubito;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        value_Ruedita = ruedita.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cubito.rotation.SetAxisAngle(cubito.rotation, value_Ruedita);
+    }
+
+    public void Funtion() {
+
+    }
+    */
 }
+
