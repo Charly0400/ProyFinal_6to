157998a45c434d28cf3f1ea4ba9e56db42b0b4e2
Charly0400 <Carlaz020400@gmail.com>
2024-05-14 09:18:32 -0600

    Update

---
+++ Assets/Scripts/VR/UIMenuManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMenuManager : MonoBehaviour
+{
+    public Animator portal_Animator;
+    public void startGame()
+    {
+        portal_Animator.Play("Open_CanvasPortal");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Salir");
+    }
+}
